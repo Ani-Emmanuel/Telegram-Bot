ac79549513735cf06771950dab4566b035d3a8bd
Emmanuel Ani <mailsforanih@gmail.com>
2020-10-22 13:47:41 +0100

    added check for string and empty spaces

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 const db = require("./db");
 const { Telegraf } = require("telegraf");
 
-const bot = new Telegraf("Your Api key here");
+const bot = new Telegraf("1304376568:AAGRm5vsC69_1ezvtqqbqxIxE4SsumQl548");
 
 bot.on("message", async (ctx, next) => {
   const info = await ctx.tg.getUpdates();
   const userId = Number(info[0].message.text.split(" ")[1]);
-  console.log(typeof userId);
-    const sql = `INSERT INTO user (userId) VALUES(${userId})`;
-      const check = `select * from user where userId = ${userId}`;
-      const param = [];
-      db.get(check, param, (err, result) => {
-        if (err) console.error(err);
-        console.log(result);
-        if (result) {
-          ctx.reply("You cannot perform this action");
-          return;
-        } else {
-          db.all(sql, param, (err, result) => {
-            if (err) console.log(err);
-            console.log(`User added successfully`);
-          });
-        }
+
+  if (isNaN(userId)) {
+    return ctx.reply(`failed!! Enter a valid number, UserId must be a number`);
+  }
+
+  const sql = `INSERT INTO user (userId) VALUES(${userId})`;
+  const check = `select * from user where userId = ${userId}`;
+  const param = [];
+  db.get(check, param, (err, result) => {
+    if (err) console.error(err);
+    if (result) {
+      ctx.reply(`${userId} is already in the database`);
+      return;
+    } else {
+      db.all(sql, param, (err, result) => {
+        if (err) console.log(err);
+        ctx.reply(`${userId} added successfully`);
       });
+    }
+  });
 });
 bot.launch();
